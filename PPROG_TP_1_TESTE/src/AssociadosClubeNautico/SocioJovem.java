adb6dcc8edd538b956f76806b58be94a4664076a
Marco Silva <1151883@isep.ipp.pt>
2017-03-26 10:36:02 +0100

    Esqueleto do trabalho

--- PPROG_TP_1_TESTE/src/AssociadosClubeNautico/SocioJovem.java
+++ PPROG_TP_1_TESTE/src/AssociadosClubeNautico/SocioJovem.java
@@ -1,14 +1,15 @@
+package AssociadosClubeNautico;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tp1;
 
 /**
  *
  * @author Marco
  */
-public class SociosAdultos {
-    //Aqui o joão tb é um tótó
+public abstract class SocioJovem extends Associados{
+    
 }
